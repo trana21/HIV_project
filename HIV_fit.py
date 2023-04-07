a77e5c80babb35726c461992aba084fa38a08706
Anh Tran <anh.3.tran@ucdenver.edu>
2023-04-07 10:52:33 -0600

    HIV_fit.py is used to fit experimental data to our viral load equation

---
+++ HIV_fit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 10:46:37 2023
+
+@author: atran
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from scipy.optimize import curve_fit
+
+# Fit experimental data to our viral load equation
+
